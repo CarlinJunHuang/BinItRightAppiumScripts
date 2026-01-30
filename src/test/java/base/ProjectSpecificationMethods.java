881d9e966c40c5ba68c6160155e16ece16f25b36
Muthu-raj22 <e1539335@u.nus.edu>
2026-01-30 22:53:41 +0530

    Initial Appium SIT framework with launch test

---
+++ src/test/java/base/ProjectSpecificationMethods.java
@@ -0,0 +1,44 @@
+package base;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.URI;
+import java.net.URL;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class ProjectSpecificationMethods {
+	
+	protected AndroidDriver driver;
+	
+	@BeforeMethod
+    public void setUp() throws Exception {
+
+        String apkPath = System.getProperty("apk.path");
+
+        if (apkPath == null) {
+            throw new RuntimeException("APK path not provided");
+        }
+
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setDeviceName("Android Emulator")
+                .setAutomationName("UiAutomator2")
+                .setApp(apkPath)
+                .autoGrantPermissions();
+
+        driver = new AndroidDriver(
+        		URI.create("http://127.0.0.1:4723").toURL(),
+                options
+        );
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
