ba80b7e99efc41b608da9a7666e9f4359576b587
raj22 <91093983+Muthuraj22@users.noreply.github.com>
2026-02-01 21:53:46 +0530

    Extend AppLaunchTest with ProjectSpecificationMethods

--- src/test/java/tests/AppLaunchTest.java
+++ src/test/java/tests/AppLaunchTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class AppLaunchTest {
 
 	@Test
-    public void appShouldLaunch() {
+    public void appShouldLaunch() extends ProjectSpecificationMethods{
         // If app launches without crash → test passes
         System.out.println("App launched successfully");
 
