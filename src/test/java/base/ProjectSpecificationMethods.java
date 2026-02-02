413b683d7007cbaf14c5810c40f2f772ae8054d0
raj22 <91093983+Muthuraj22@users.noreply.github.com>
2026-02-02 14:27:05 +0530

    Add Duration import to ProjectSpecificationMethods

--- src/test/java/base/ProjectSpecificationMethods.java
+++ src/test/java/base/ProjectSpecificationMethods.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeMethod;
 
 import java.net.URI;
 import java.net.URL;
+import java.time.Duration;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
